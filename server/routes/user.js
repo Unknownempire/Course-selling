4c69c64f49d3e8f30c1fa924053c28545e47aca1
Pranav Chaudhary <playerpranav1@gmail.com>
2024-02-26 01:38:45 +0530

    comment removal

--- server/routes/user.js
+++ server/routes/user.js
@@ -47,14 +47,10 @@ router.get('/courses', authenticateJwt, async (req, res) => {
 
 router.post('/courses/:courseId', authenticateJwt, async (req, res) => {
   const name = req.body.username;
-  // console.log(req.user.username);
-  // const credit = await Credit.findOne({username:name,...req.body});
   const credit = await Credit.findOne(req.body);
   if (!credit) {
     res.json({ message: "No credit Card found" });
   } else {
-    // console.log(name);
-    // res.json({message : "credit card found"});
     console.log(credit.cardNumber);
     const course = await Course.findById(req.params.courseId);
     if (course) {
@@ -62,12 +58,11 @@ router.post('/courses/:courseId', authenticateJwt, async (req, res) => {
       if (user) {
         // checking whether course already purchased or not?
         console.log("user id = " +  user._id);
-        // const purchased = await User.findOne({ purchasedCourses: req.params.courseId });
+
         const purchased = await User.findOne({
           _id: user._id,
           purchasedCourses: { $in: [req.params.courseId] }
         });
-        // const purchased = await User.findById({purchasedCourses: req.params.courseId});
 
         if (purchased) {
           // res.json({ message: 'Course already purchased' });
@@ -77,7 +72,6 @@ router.post('/courses/:courseId', authenticateJwt, async (req, res) => {
           await user.save();
           res.json({ message: 'Course purchased successfully' });
         }
-        // console.log("code done");
       } else {
         res.status(403).json({ message: 'User not found' });
       }
