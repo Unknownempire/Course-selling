b200a3d1d44a540b38b49a940f99ad3ca44fd93b
Pranav Chaudhary <playerpranav1@gmail.com>
2024-03-22 23:18:41 +0530

    setting courseid in local storage on clicking the view button

--- admin-client/src/components/admin-components/Courses.jsx
+++ admin-client/src/components/admin-components/Courses.jsx
@@ -1,6 +1,6 @@
 import { Button, Card, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { BASE_URL } from "../../config.js";
 import { isadminState } from "../../store/atoms/isadmin.js";
@@ -22,20 +22,21 @@ function Courses() {
         init();
     }, []);
 
-    return <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+    return <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
         {courses.map(course => {
-            return <Course course={course} />}
+            return <Course course={course} />
+        }
         )}
     </div>
 }
 
-export function Course({course}) {
+export function Course({ course }) {
     const navigate = useNavigate();
     const isAdmin = useRecoilValue(isadminState);
-    const [purchasedCourses,setPurchasedCourses] = useState([])
+    const [purchasedCourses, setPurchasedCourses] = useState([])
     let purchased = 'purchase'
 
-    if(!isAdmin) {
+    if (!isAdmin) {
         const init = async () => {
             const response = await axios.get(`${BASE_URL}/user/purchasedCourses/`, {
                 headers: {
@@ -76,17 +77,17 @@ export function Course({course}) {
         <Typography textAlign={"center"} variant="h5">{course.title}</Typography>
         <Typography textAlign={"center"} variant="subtitle1">{course.description}</Typography>
         <div style={{
-            display:'flex',
-            justifyContent:'center',
-            alignItems:'center',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
             // border:'2px solid red',
-            padding:20,
+            padding: 20,
         }}>
-        <img src={course.imageLink} style={{
-            width: 300,
-            height: 200,
-            marginBottom: '20px',
-        }} ></img>
+            <img src={course.imageLink} style={{
+                width: 300,
+                height: 200,
+                marginBottom: '20px',
+            }} ></img>
         </div>
         <div style={{
             // border:'2px solid red',
@@ -99,40 +100,52 @@ export function Course({course}) {
             right: 0,
             textAlign: 'center'
         }}>
-        <div style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: 20,
-        }}>
-            {/* <Button variant="contained" size="large" onClick={() => {
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: 20,
+            }}>
+                {/* <Button variant="contained" size="large" onClick={() => {
                 navigate("/course/" + course._id);
             }}>Edit</Button> */}
 
-            {isAdmin ? (
-                <Button variant="contained" size="large" onClick={() => {
-                    // navigate("/course/" + course._id);
-                    alert("Contents Coming Soon..")
-                }}>View</Button>
-             ) : (
+                {isAdmin ? (
+                    <Button variant="contained" size="large" onClick={() => {
+                        // navigate("/course/" + course._id);
+                        alert("Contents Coming Soon..")
+                    }}>View</Button>
+                ) : (
                     purchased === 'purchase' ? (
                         <Button variant="contained" size="large" onClick={async () => {
                             const courseId = course._id;
                             navigate("/payment/" + courseId);
                         }}>Purchase</Button>
                     ) : (
-                        <Button variant="contained" color="success" size="large" onClick={async() => {
+                        <Button variant="contained" color="success" size="large" onClick={async () => {
                             const response = await axios.get(`${BASE_URL}/user/learn/` + String(course._id), {
                                 headers: {
                                     Authorization: `Bearer ${localStorage.getItem('token')}`
                                 }
                             })
                             const route = response.data.message
-                            // alert('route ' + route);
+                            localStorage.setItem('courseid', course._id);
+                            // const courseId = localStorage.getItem('courseid');
+                            // const resAttempt = await axios.get(`${BASE_URL}/user/learn/${courseId}/attempt`, {
+                            //     headers: {
+                            //         Authorization: `Bearer ${localStorage.getItem('token')}`
+                            //     }
+                            // });
+
+                            // console.log(resAttempt);
+
                             navigate("/course/" + route);
+                            // alert('route ' + route);
+                            // navigate("/course/" + route);
+                            // console.log(course._id);
                         }}>View</Button>
                     )
                 )}
-        </div>
+            </div>
         </div>
     </Card>
 
