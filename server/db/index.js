9408c54a8396252fc7565b43ac7edffb20bdaf1f
Pranav Chaudhary <playerpranav1@gmail.com>
2024-05-09 16:49:09 +0530

    feat: fullName field added in userSchema

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
+    fullName: String,
     purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
   });
   
